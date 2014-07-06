9c7f53ddc1e5e4d4422fbf2ce2310caf1322e26c
yiilover <yiilover@gmail.com>
2014-07-06 19:36:07 +0800

    display none

--- template/cmszs_360bbs/common/footer.php
+++ template/cmszs_360bbs/common/footer.php
@@ -177,10 +177,11 @@
 	</script>
 <!--{/if}-->
 <!--{eval output();}-->
+<div style="display: none">
 <script type="text/javascript">
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F5e254ca90e23098ed41dc9dd4d4024ca' type='text/javascript'%3E%3C/script%3E"));
 </script>
-
+</div>
 </body>
 </html>
